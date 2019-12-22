1bfff9754547472153665926ee1a30df03746103
BosEriko <bos.eriko@gmail.com>
2019-12-22 16:13:20 +0800

    add a clear message on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Bos Eriko fans!</h1>
+    <p>I'll be putting all my apps here!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
